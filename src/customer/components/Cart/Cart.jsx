f535165f3d9cb5a7789000e1a101a0c8231c8af9
parth <parthzadafiya13@gmail.com>
2024-02-28 16:22:11 +0530

    7 commit

--- src/customer/components/Cart/Cart.jsx
+++ src/customer/components/Cart/Cart.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Cartitem from './Cartitem'
 import { Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCart } from '../../../State/Cart/Action'
 
 const Cart = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const { cart } = useSelector(store => store)
+    const dispatch = useDispatch();
 
-    const handleCheckout=()=>{
+    useEffect(() => {
+        dispatch(getCart());
+    }, [cart.updateCartItem,cart.deleteCartItem])
+
+    const handleCheckout = () => {
         navigate("/checkout?step=2")
     }
     return (
@@ -14,7 +22,7 @@ const Cart = () => {
 
             <div className='lg:grid grid-cols-3 lg:px-16 relative'>
                 <div className=' col-span-2'>
-                    {[1,1,1,1].map((item)=><Cartitem />)}
+                    {cart.cart?.cartItems.map((item) => <Cartitem item={item} key={item.id}/>)}
                 </div>
 
                 <div className='px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0'>
@@ -26,12 +34,12 @@ const Cart = () => {
 
                             <div className='flex justify-between pt-3 text-black'>
                                 <span>Price</span>
-                                <span>₹699</span>
+                                <span>₹{cart.cart?.totalPrice}</span>
                             </div>
 
                             <div className='flex justify-between pt-3'>
                                 <span>Disccount</span>
-                                <span className='text-green-600'>-₹359</span>
+                                <span className='text-green-600'>-₹{cart.cart?.discounte}</span>
                             </div>
 
                             <div className='flex justify-between pt-3 '>
@@ -41,7 +49,8 @@ const Cart = () => {
 
                             <div className='flex justify-between pt-3 font-bold'>
                                 <span>Totle Amount</span>
-                                <span className='text-green-600'>₹699</span>
+                                <span className='text-green-600'>₹{cart.cart?.totalDiscountedPrice
+                                }</span>
                             </div>
 
                         </div>
--- src/customer/components/Cart/Cartitem.jsx
+++ src/customer/components/Cart/Cartitem.jsx
@@ -2,26 +2,39 @@ import { Button, IconButton } from '@mui/material'
 import React from 'react'
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useDispatch } from 'react-redux';
+import { removeCartItem, updateCartItem } from '../../../State/Cart/Action';
 
-const Cartitem = () => {
+const Cartitem = ({item}) => {
+    const dispatch=useDispatch();
+
+    const handleUpdateCartItem=(num)=>{
+        const data={data:{quantity:item.quantity+num},cartItemId:item?._id};
+        dispatch(updateCartItem(data));
+    }
+
+    const handleRemoveCartItem=()=>{
+        dispatch(removeCartItem(item._id))
+    }
+
     return (
-        <div className='p-5 shadow-lg border rounded-md'>
+        <div className='p-5 shadow-lg border rounded-md' key={item.id}>
 
             <div className='flex items-center'>
 
                 <div className=' w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
-                    <img className=' w-full h-full object-cover object-top' src="https://rukminim1.flixcart.com/image/612/612/xif0q/jean/1/4/v/-original-imaggjcyhzz4myqh.jpeg?q=70" alt="" />
+                    <img className=' w-full h-full object-cover object-top' src={item.product.imageUrl} alt="" />
                 </div>
 
                 <div className='mt-5 space-y-1'>
-                    <p className=' font-semibold'>Men Slim Mid Rise Dark Blue Jeans</p>
-                    <p className=' opacity-70'>Size: L,Dark Blue</p>
-                    <p className='opacity-70 mt-2'>Seller: BEN MARTIN</p>
+                    <p className=' font-semibold'>{item.product.title}</p>
+                    <p className=' opacity-70'>Size: {item.size}</p>
+                    <p className='opacity-70 mt-2'>Seller: {item.product.brand}</p>
 
                     <div className='flex space-x-5 items-center text-gray-900 pt-5'>
-                        <p className=' font-semibold'>₹399</p>
-                        <p className=' opacity-50 line-through'>₹999</p>
-                        <p className='text-green-600 font-semibold'>30% Off</p>
+                        <p className=' font-semibold'>₹{item.price}</p>
+                        <p className=' opacity-50 line-through'>₹{item.discountedPrice}</p>
+                        <p className='text-green-600 font-semibold'>{item.discountPercent}% Off</p>
                     </div>
 
                 </div>
@@ -29,19 +42,19 @@ const Cartitem = () => {
             <div className='lg:flex items-center lg:space-x-10 pt-4'>
 
                 <div className='flex item-center space-x-2'>
-                    <IconButton>
+                    <IconButton onClick={()=>handleUpdateCartItem(-1)} disabled={item.quantity<=1}>
                         <RemoveCircleOutlineIcon />
                     </IconButton>
 
-                    <span className='py-1 px-7 border rounded-sm' >3</span>
-                        <IconButton sx={{color:"RGB(145 85 253)"}}>
+                    <span className='py-1 px-7 border rounded-sm' >{item.quantity}</span>
+                        <IconButton onClick={()=>handleUpdateCartItem(1)} sx={{color:"RGB(145 85 253)"}}>
                             <AddCircleOutlineIcon />
                         </IconButton>
                     
                 </div>
 
                 <div>
-                    <Button sx={{color:"RGB(145 85 253)"}}>Remove</Button>
+                    <Button onClick={handleRemoveCartItem} sx={{color:"RGB(145 85 253)"}}>Remove</Button>
                 </div>
             </div>
 
